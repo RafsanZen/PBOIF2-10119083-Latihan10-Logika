d6f58ff0a4022a1c80ac5240da23d6d37933a8c5
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 19:12:09 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan10-Logika/src/pboif2/pkg10119083/latihan10/logika/PBOIF210119083Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * Nama      : Rafsan Zen Mustaofa
+ * Kelas     : IF2
+ * NIM       : 10119083
+ * Deskripsi : Menampilkan logika
+ */
+public class PBOIF210119083Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=20);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
